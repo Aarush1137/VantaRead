72076edb816149d1c34b944270a5031e522de6c4
Aarush Jain <aarushjain890@gmail.com>
2026-07-10 03:22:35 +0530

    Auto Backup 2026-07-10 03:22:35

--- app/src/main/java/com/example/vantaread/MainActivity.kt
+++ app/src/main/java/com/example/vantaread/MainActivity.kt
@@ -8,8 +8,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
 import com.example.vantaread.theme.VantaReadTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
